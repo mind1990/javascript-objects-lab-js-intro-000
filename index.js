14363008df55db7ec37d4453c4519b7e6a924269
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 20:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, vallue) {
-  
+  object = { [key]: value }
+  return object
 }
