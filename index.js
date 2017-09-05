9a33d6f200cdd935c4bb5b2842d09be652e7c6b0
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 21:19:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-//function deleteFromObjectByKey(object, key) {
-
-//}
+function deleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
 
 
 function destructivelyDeleteFromObjectByKey(object, key) {
